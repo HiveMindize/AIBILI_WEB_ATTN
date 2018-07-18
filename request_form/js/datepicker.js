605ec2709a586d631b90031de9e3e9a07a6b2420
David Coimbra <david.coimbra@tecnico.ulisboa.pt>
2018-07-18 16:53:14 +0100

    Initial files
    Form with interactive calendar

---
+++ request_form/js/datepicker.js
@@ -0,0 +1,11 @@
+$(function() {
+  $('input[name="dates"]').daterangepicker({
+    timePicker: true,
+    timePicker24Hour: true,
+    startDate: moment().startOf('hour'),
+    endDate: moment().startOf('hour').add(32, 'hour'),
+    locale: {
+      format: 'DD/MM/YYYY hh:mm'
+    }
+  });
+});
